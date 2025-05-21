1f7ef5549c59e2951a6a04a30d4bfb78b9d19909
carlos <carlosajcip511@gmail.com>
2025-05-21 11:02:47 -0500

    Se agrego la url a la barra de navegacion

--- resources/views/components/navigation-menu.blade.php
+++ resources/views/components/navigation-menu.blade.php
@@ -7,75 +7,7 @@
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Panel
                 </a>
-                {{-- <div class="sb-sidenav-menu-heading">Interface</div> --}}
-                {{-- <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                    Layouts
-                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a>
-                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                    <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="layout-static.html">Static Navigation</a>
-                        <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
-                    </nav>
-                </div>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                    <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                    Pages
-                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a>
-                <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                    <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                            Authentication
-                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                        </a>
-                        <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                            <nav class="sb-sidenav-menu-nested nav">
-                                <a class="nav-link" href="login.html">Login</a>
-                                <a class="nav-link" href="register.html">Register</a>
-                                <a class="nav-link" href="password.html">Forgot Password</a>
-                            </nav>
-                        </div>
-                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                            Error
-                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                        </a>
-                        <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                            <nav class="sb-sidenav-menu-nested nav">
-                                <a class="nav-link" href="401.html">401 Page</a>
-                                <a class="nav-link" href="404.html">404 Page</a>
-                                <a class="nav-link" href="500.html">500 Page</a>
-                            </nav>
-                        </div>
-                    </nav>
-                </div> --}}
                 <div class="sb-sidenav-menu-heading">Modulos</div>
-                <a class="nav-link" href="{{route('categoria.index')}}">
-                    <div class="sb-nav-link-icon"><i class="fa-solid fa-list"></i></div>
-                    Categorias
-                </a>
-                <a class="nav-link" href="{{route('marca.index')}}">
-                    <div class="sb-nav-link-icon"><i class="fa-solid fa-tags"></i></div>
-                    Marcas
-                </a>
-                <a class="nav-link" href="{{route('presentaciones.index')}}">
-                    <div class="sb-nav-link-icon"><i class="fa-regular fa-file"></i></i></div>
-                    Presentaciones
-                </a>
-                <a class="nav-link" href="{{route('producto.index')}}">
-                    <div class="sb-nav-link-icon"><i class="fa-solid fa-store"></i></div>
-                    Productos
-                </a>
-                <a class="nav-link" href="{{route('cliente.index')}}">
-                    <div class="sb-nav-link-icon"><i class="fa-solid fa-users"></i></div>
-                    Clientes
-                </a>
-                <a class="nav-link" href="{{route('proveedores.index')}}">
-                    <div class="sb-nav-link-icon"><i class="fa-solid fa-user"></i></div>
-                    Proveedores
-                </a>
-
                    {{-- <div class="sb-sidenav-menu-heading">Interface</div> --}}
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-store"></i></div>
@@ -89,6 +21,43 @@
                     </nav>
                 </div>
             
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayoutss" aria-expanded="false" aria-controls="collapseLayoutss">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-cart-plus"></i></div>
+                    Ventas
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseLayoutss" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="{{route('venta.index')}}">Ver</a>
+                        <a class="nav-link" href="{{route('venta.create')}}">Crear</a>
+                    </nav>
+                </div>
+                <a class="nav-link" href="{{route('categoria.index')}}">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-list"></i></div>
+                    Categorias
+                </a>
+                <a class="nav-link" href="{{route('marca.index')}}">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-tags"></i></div>
+                    Marcas
+                </a>
+                <a class="nav-link" href="{{route('presentaciones.index')}}">
+                    <div class="sb-nav-link-icon"><i class="fa-regular fa-file"></i></i></div>
+                    Presentaciones
+                </a>
+                <a class="nav-link" href="{{route('producto.index')}}">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-seedling"></i></div>
+                    Productos
+                </a>
+                <a class="nav-link" href="{{route('cliente.index')}}">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-users"></i></div>
+                    Clientes
+                </a>
+                <a class="nav-link" href="{{route('proveedores.index')}}">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-user"></i></div>
+                    Proveedores
+                </a>
+
+                
             </div>
         </div>
         <div class="sb-sidenav-footer">
